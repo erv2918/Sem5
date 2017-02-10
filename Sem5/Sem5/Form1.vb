87c0bcd1d9cc0e69c4cf52dae15eb23044557f31
Estudiante <Estudiante@1303-04>
2017-02-09 20:39:45 -0600

    Ejercicio 2 Listo

--- Sem5/Sem5/Form1.vb
+++ Sem5/Sem5/Form1.vb
@@ -14,10 +14,20 @@
     End Sub
 
     Private Sub btnFuncion2_Click(sender As Object, e As EventArgs) Handles btnFuncion2.Click
-
+        Dim vec() As String = Split(txtPalabra.Text, ",")
+        txtResultado.Text = Ejercicio2(vec(0), (vec(1)))
     End Sub
-    Function Ejercicio2(palabra As String, letra As Char) As Integer
+    Function Ejercicio2(palabra As String, letra As String) As Integer
         Dim posicion As Integer = -1
+        Dim index As Integer = 1
+        Dim bandera As Boolean = False
+        While index <= palabra.Length And bandera = False
+            If letra = Mid(palabra, index, 1) Then
+                posicion = index
+                bandera = True
+            End If
+            index += 1
+        End While
 
         Return posicion
     End Function
